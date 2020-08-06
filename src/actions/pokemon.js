b7696db03fa3869a7a5b32a645329c6d450e62e0
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-06 18:45:42 +1000

    basic actions added, began writing fetch function to search for pokemon

--- src/actions/pokemon.js
+++ src/actions/pokemon.js
@@ -2,8 +2,18 @@ export const FETCH_POKEMON_START = "FETCH_POKEMON_START";
 export const FETCH_POKEMON_SUCCESS = "FETCH_POKEMON_SUCCESS";
 export const FETCH_POKEMON_FAILURE = "FETCH_POKEMON_FAILURE";
 
-const fetchPokemonStart = () => {};
+const fetchPokemonStart = () => ({
+  type: FETCH_POKEMON_START,
+});
 
-const fetchPokemonSuccess = () => {};
+const fetchPokemonSuccess = (pokemon) => ({
+  type: FETCH_POKEMON_SUCCESS,
+  payload: { pokemon },
+});
 
-const fetchPokemonFailure = () => {};
+const fetchPokemonFailure = (error) => ({
+  type: FETCH_POKEMON_FAILURE,
+  payload: { error },
+});
+
+export const fetchPokemon = () => {};
