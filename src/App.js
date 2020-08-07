36a6f9134794fa5d541c375ccb140f10f0535ed4
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-07 12:19:37 +1000

    adding pokedex image

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from "react";
 
 function App() {
   return (
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=50").then((response) =>
-      console.log(response)
-    ),
-    (
-      <main className="test">
-        <h1>Pokèdex</h1>
-        <h2>Using Pokèmon api</h2>
-      </main>
-    )
+    // fetch("https://pokeapi.co/api/v2/pokemon?limit=50").then((response) =>
+    //   console.log(response)
+    // ),
+    <main className="test">
+      <div className="pokedex"></div>
+    </main>
   );
 }
 
