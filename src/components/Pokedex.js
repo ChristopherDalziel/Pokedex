0191efeae9076607db2daa33c13ac8cd78fbde2c
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-09 12:04:16 +1000

    displaying sprite from api, currently loading hook isn't working

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -3,24 +3,36 @@ import axios from "axios";
 
 const Pokedex = () => {
   const [data, setData] = useState({ pokemon: [] });
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const result = await axios("https://pokeapi.co/api/v2/pokemon/1");
 
       setData(result.data);
     };
 
     fetchData();
+    setIsLoading(false);
   }, []);
 
-  console.log(data);
+  console.log("loading? : ", isLoading);
+  console.log(data.sprites);
 
   return (
-    <div className="pokedex">
-      <div>{data.name}</div>
-      <img src={data.sprites.map((sprite) => sprite.front_default)} />
-    </div>
+    <>
+      {/* <div className="pokedex"> */}
+      {isLoading ? (
+        <p>Loading..</p>
+      ) : (
+        <>
+          <div>{data.name}</div>
+          <img src={data.sprites?.front_default} alt="poke sprite" />
+        </>
+      )}
+      {/* </div> */}
+    </>
   );
 };
 
