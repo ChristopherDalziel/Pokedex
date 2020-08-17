79df2a4290c044ff52fa126def5345d7a1c27b5b
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-17 11:32:38 +1000

    Added a render before each test

--- src/__tests__/pokedex.js
+++ src/__tests__/pokedex.js
@@ -3,11 +3,17 @@ import { render, fireEvent, screen } from "@testing-library/react";
 
 import Pokedex from "../components/Pokedex";
 
+beforeEach(() => {
+  render(<Pokedex />);
+});
+
 describe("Pokèdex", () => {
   test("Renders Pokèdex component in it's 'off' state", () => {
-    render(<Pokedex />);
-
     expect(screen.getByTestId("Pokedex")).toBeInTheDocument();
     expect(screen.getByText("On")).toBeInTheDocument();
   });
+
+  // test("Switches Pokèdex 'on'", () => {
+  //   render;
+  // });
 });
