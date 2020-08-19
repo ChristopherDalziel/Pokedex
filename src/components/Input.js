c67136c7a45160c2091a161e3b0643fbb86e8a47
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-19 11:21:39 +1000

    added react import back (oops), looking into creating smaller components for each section of the pokedex

---
+++ src/components/Input.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Input = (pokemonId) => {};
+
+export default Input;
--- src/utils/errorHandling.js
+++ src/utils/errorHandling.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const errorHandling = (error) => {
   if (error === "Response") {
     return <p>Pokemon not found, Please try again.</p>;
