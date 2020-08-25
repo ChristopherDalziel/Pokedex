57658463f38e75ce1df56a840f3d5cfde8ac9587
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-25 14:10:45 +1000

    fixed error that crashed the app when searched a pokemon by name instead of number and then pressed + because the app was trying to + a string

--- src/components/Dpad.js
+++ src/components/Dpad.js
@@ -2,20 +2,21 @@ import React, { useContext } from "react";
 import { PokedexContext } from "../context/PokedexContext";
 
 const Dpad = () => {
-  const { pokemonId, setPokemonId } = useContext(PokedexContext);
+  const { pokemonId, setPokemonId, pokemon } = useContext(PokedexContext);
+
   return (
     <span>
       <button
         data-testid="negativeButton"
         className="pokedex--negativeButton"
-        onClick={() => setPokemonId(pokemonId - 1)}
+        onClick={() => setPokemonId(pokemon.id - 1)}
       >
         -
       </button>
       <button
         data-testid="positiveButton"
         className="pokedex--positiveButton"
-        onClick={() => setPokemonId(pokemonId + 1)}
+        onClick={() => setPokemonId(pokemon.id + 1)}
       >
         +
       </button>
