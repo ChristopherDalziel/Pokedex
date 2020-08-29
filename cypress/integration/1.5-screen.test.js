a9cef867aa0cc106c465a29fe670da47b226a423
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-29 11:21:08 +1000

    screen cypress tests

--- cypress/integration/1.5-screen.test.js
+++ cypress/integration/1.5-screen.test.js
@@ -1,3 +1,39 @@
 describe("Checks the correct data is displayed to the screen", () => {
-  test("", () => {});
+  it("Checks screen is displayed", () => {
+    cy.get(".pokedex--screen").should("be.visible");
+  });
+
+  it("Checks that default Pokemon name is displayed", () => {
+    cy.get(".pokedex--screen__name")
+      .should("be.visible")
+      .should("have.text", "bulbasaur");
+  });
+
+  it("Checks the default Pokemon # is displayed", () => {
+    cy.get(".pokedex--screen__pokeNumber")
+      .should("be.visible")
+      .should("have.text", "#001");
+  });
+
+  it("Checks the default Pokemons sprite is displayed", () => {
+    cy.get(".pokedex--screen__sprite")
+      .should("be.visible")
+      .should("have.attr", "src")
+      .should(
+        "include",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+      );
+  });
+
+  it("Checks the default Pokemons shiny sprite is displayed", () => {
+    cy.get(".pokedex--shinyButton").click();
+
+    cy.get(".pokedex--screen__sprite")
+      .should("be.visible")
+      .should("have.attr", "src")
+      .should(
+        "include",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png"
+      );
+  });
 });
