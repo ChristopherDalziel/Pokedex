729f43b04bd53dcc0a3b8b1e2e7f6753d9dc3acd
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-29 14:41:43 +1000

    abilities test

--- src/__tests__/components/abilities.js
+++ src/__tests__/components/abilities.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { PokedexContext } from "../../context/PokedexContext";
+import pokemonData from "../__fixtures__/pokemon";
 import Abilities from "../../components/Abilities";
 
-describe("Abilities component", () => {
-  test("Displays a Pokemons abilities", () => {
-    const { getByTestId } = render(<Abilities />);
+describe("Stats component", () => {
+  test("Checks ability name", () => {
+    const context = {};
+
+    context.pokemon = pokemonData[0];
+
+    const { getByTestId } = render(
+      <PokedexContext.Provider value={context}>
+        <Abilities />
+      </PokedexContext.Provider>
+    );
+
+    expect(getByTestId("abilities")).toHaveTextContent(
+      pokemonData[0].abilities[0].ability.name
+    );
   });
 });
