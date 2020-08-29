107fdecf53525dde6b4c30feec00c1a4334d0336
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-29 15:10:15 +1000

    remove comments from Pokedex component

--- src/__tests__/components/pokedex.js
+++ src/__tests__/components/pokedex.js
@@ -1,47 +1,14 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import { PokedexProvider, PokedexContext } from "../../context/PokedexContext";
-import pokemonData from "../__fixtures__/pokemon";
-
+import { render, screen } from "@testing-library/react";
 import Pokedex from "../../components/Pokedex";
-import PowerButton from "../../components/PowerButton";
 
-describe("Pokèdex", () => {
+beforeEach(() => {
+  render(<Pokedex />);
+});
+
+describe("Pokèdex component", () => {
   test("Renders Pokèdex component in it's 'off' state", () => {
-    const { getByTestId, getByText } = render(<Pokedex />);
-
-    expect(getByTestId("Pokedex")).toBeInTheDocument();
-    expect(getByText("On")).toBeInTheDocument();
+    expect(screen.getByTestId("Pokedex")).toBeInTheDocument();
+    expect(screen.getByText("On")).toBeInTheDocument();
   });
-
-  // test("Sets setPower to true and switches Pokèdex on", () => {
-  //   const TestComponent = () => {
-  //     const { power, setPower } = React.useContext(PokedexContext);
-
-  //     return (
-  //       <PowerButton />
-  //       // <>
-  //       //   <button data-testid="power" onClick={() => setPower(!power)}>
-  //       //     {power ? "Off" : "On"}
-  //       //   </button>
-  //       // </PowerButton>
-  //     );
-  //   };
-
-  //   const { getByTestId } = render(
-  //     <PokedexContext.Prover>
-  //       <PowerButton />
-  //     </PokedexContext.Prover>
-  //   );
-
-  //   const button = getByTestId("power");
-  //   fireEvent.click(button);
-
-  //   // const { findByTestId } = render(<TestComponent />);
-  //   // expect(findByTestId("power")).toBeInTheDocument();
-  // });
-
-  // test("Renders Pokèdex with data", () => {
-  //   const pokemon = { pokemonData };
-  // });
 });
--- src/components/Lights.js
+++ src/components/Lights.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Lights = () => {
   return (
-    <>
+    <div data-testid="lights">
       <div className="pokedex--light" />
       <div className="pokedex--light__red" />
       <div className="pokedex--light__yellow" />
       <div className="pokedex--light__green" />
       <div className="pokedex--light__yellow_big" />
-    </>
+    </div>
   );
 };
 
