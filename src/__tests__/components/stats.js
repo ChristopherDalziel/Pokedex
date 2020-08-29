cdae18be7efe79705cd0b5a000a0e9d8d7b36cee
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-08-29 14:38:37 +1000

    stats test

--- src/__tests__/components/stats.js
+++ src/__tests__/components/stats.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { PokedexContext } from "../../context/PokedexContext";
 import Stats from "../../components/Stats";
+import pokemonData from "../__fixtures__/pokemon";
 
 describe("Stats component", () => {
-  test("Displays a Pokemons stats", () => {
-    const { getByTestId } = render(<Stats />);
+  test("Checks first stat name and base_stat displayed in Stats component", () => {
+    const context = {};
+
+    context.pokemon = pokemonData[0];
+
+    const { getByTestId } = render(
+      <PokedexContext.Provider value={context}>
+        <Stats />
+      </PokedexContext.Provider>
+    );
+
+    expect(getByTestId("stats")).toHaveTextContent(
+      pokemonData[0].stats[0].stat.name
+    );
+    expect(getByTestId("stats")).toHaveTextContent(
+      pokemonData[0].stats[0].base_stat
+    );
   });
 });
