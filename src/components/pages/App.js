b0111973e9f35044405a30075f3fbd97642510af
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-09-01 12:13:23 +1000

    installed fontawesome icon, updated toggle button colour, added github project link and icon, added link css and title

--- src/components/pages/App.js
+++ src/components/pages/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { PokedexProvider } from "../../context/PokedexContext";
+
 import Pokedex from "../Pokedex";
 import Tutorial from "../Tutorial";
 
@@ -9,6 +13,14 @@ const App = () => {
       <main className="container">
         <Pokedex />
         <Tutorial />
+        <a
+          className="github"
+          target="_blank"
+          href="https://github.com/ChristopherDalziel/Pokedex"
+          title="Link to projects GitHub repository"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
       </main>
     </PokedexProvider>
   );
